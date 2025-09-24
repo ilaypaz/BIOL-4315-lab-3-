38f11c80642a1a8cc83c54c843b5b4ebd49092d2
‘ilaypaz’ <pazlilay@gmail.com>
2025-09-24 15:46:05 -0700

    QUESTION 6 COMMIT

---
+++ backup.R
@@ -0,0 +1,41 @@
+library("IRanges")
+library("GenomicRanges")
+library("Rsamtools")
+library("GenomicAlignments")
+library("Gviz")
+#Download the reference genome from refseq
+
+
+library(Biostrings)
+genome<-readDNAStringSet("/Users/ipaz00/Downloads/BIOL4315_R/biol 4315 lab3/BIOL-4315-lab-3-/GCF_000146045.2_R64_genomic.fna")
+names(genome)
+xome1<- genome[grep("chromosome I",names(genome), ignore.case=TRUE)]
+writeXStringSet(xome1, "R64_chr1.fna")
+xome1
+
+library(Rsamtools)
+library(GenomicAlignments)
+library(GenomicRanges)
+library(Gviz)
+# BAM file path
+bam_file <- "alignment.sorted.bam"
+# Open connection to BAM file
+bam <- BamFile(bam_file)
+
+# Read all alignments (already restricted to chr1)
+alns <- readGAlignments(bam)
+options(ucscChromosomeNames = FALSE)
+
+# Convert to GRanges object
+gr_alns <- granges(alns)
+aln_track <- AnnotationTrack(gr_alns, name = "Contigs", genome = "sacCer3", chromosome = "NC_001133.9")
+
+
+# Add genome axis for scale
+axis_track <- GenomeAxisTrack()
+# Plot
+plotTracks(list(axis_track, aln_track),
+           from = min(start(gr_alns)),
+           to = max(end(gr_alns)),
+           main = "Contig Alignments to Chromosome 1")
+
