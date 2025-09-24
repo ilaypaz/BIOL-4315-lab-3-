2e85d31fe42bf36b94a0e4ee193f90ce8e423da6
‘ilaypaz’ <pazlilay@gmail.com>
2025-09-24 16:19:51 -0700

    f

--- backup.R
+++ backup.R
@@ -39,3 +39,33 @@ plotTracks(list(axis_track, aln_track),
            to = max(end(gr_alns)),
            main = "Contig Alignments to Chromosome 1")
 
+fastaroni<-readDNAStringSet("/Users/ipaz00/Downloads/BIOL4315_R/biol 4315 lab3/BIOL-4315-lab-3-/assembly.fasta")
+length<-width(fastaroni)
+fileinclusion<-fastaroni[which.max(length)]
+writeXStringSet(fileinclusion,"longest_contig.fasta")
+
+
+library(Rsamtools)
+library(GenomicAlignments)
+library(GenomicRanges)
+library(Gviz)
+# BAM file path
+bam_file <- "cds_aln.sorted.bam"
+# Open connection to BAM file
+bam <- BamFile(bam_file)
+
+# Read all alignments (already restricted to chr1)
+alns <- readGAlignments(bam)
+options(ucscChromosomeNames = FALSE)
+
+# Convert to GRanges object
+gr_alns <- granges(alns)
+aln_track <- AnnotationTrack(gr_alns, name = "Contigs", genome = "sacCer3", chromosome = "contig_26")
+
+# Add genome axis for scale
+axis_track <- GenomeAxisTrack()
+# Plot
+plotTracks(list(axis_track, aln_track),
+           from = min(start(gr_alns)),
+           to = max(end(gr_alns)),
+           main = "CDS alignments to Contig 26 ")
